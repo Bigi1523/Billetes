2b46b4197a2a6bc7a642a8d65fd9c57835ec6121
Bigi <big2468a@hotmail.com>
2026-02-28 23:58:47 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("serial-scanner").then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./ranges.json"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
